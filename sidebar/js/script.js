1e7705992d668c37f1b3bbac82cadf8729af1ef7
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-02-28 20:02:26 -0300

    Primeiro Commit

--- sidebar/js/script.js
+++ sidebar/js/script.js
@@ -1,7 +1,1 @@
 const openBTN = document.querySelector('#open-btn');
-
-openBTN.addEventListener('click', ()=>{
-    const sidebar = document.querySelector('#sidebar');
-
-    sidebar.classList.toggle('open-sidebar');
-});
