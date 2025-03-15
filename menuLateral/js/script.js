a1341c8479fdb6d29d1cfca354d2942ffe0c2a2e
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-03-15 17:26:36 -0300

    Update script.js

--- menuLateral/js/script.js
+++ menuLateral/js/script.js
@@ -11,4 +11,35 @@ openBtn.addEventListener('click', () => {
         openBtn.classList.remove('bi-arrow-right-circle-fill');
         openBtn.classList.add('bi-arrow-left-circle-fill');
     }
-})
+});
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        
+        620:{
+            slidesPerView:2
+        },
+
+        1024:{
+            slidesPerView:3
+        }
+    }
+});
